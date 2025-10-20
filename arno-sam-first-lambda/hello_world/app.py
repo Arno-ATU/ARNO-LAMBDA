ec62fef02dbe4ae77ea4f7daf34f6bb65fa1d7aa
Arno Moelich <arno.moelich@atu.ie>
2025-10-20 18:43:50 +0000

    Add event to display personall name inside Lambda handler, grabbed from event.json

--- arno-sam-first-lambda/hello_world/app.py
+++ arno-sam-first-lambda/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello from " + socket.gethostname(),
-            # "location": ip.text.replace("\n", "")
+            "message": 'hello ' + event["name"] + ' from ' + socket.gethostname(),
+
         }),
     }
